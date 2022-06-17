f5667ae2e9cfeaa0afe7fcfafbd622ffdf6ecd16
박중현 <hoodboy@Hoodboys-MacBookPro.local>
2022-06-17 16:03:00 +0900

    feat: add file

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var http = require('http');
-var url = require('url');
-var topic = require('./lib/topic');
-var author = require('./lib/author');
+var http = require("http");
+var url = require("url");
+var topic = require("./lib/topic");
+var author = require("./lib/author");
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
@@ -12,55 +12,41 @@ var app = http.createServer(function (request, response) {
     if (queryData.id === undefined) {
       //홈 화면
       topic.home(response, request);
-    }
-    else {
+    } else {
       //서브(상세) 화면
       topic.sub(response, request, queryData);
     }
   } else if (pathname === "/create") {
     //작성
     topic.createForm(response, request);
-
   } else if (pathname === "/create_process") {
     //작성 처리
     topic.createProcess(response, request);
-  }
-  else if (pathname === "/update") {
+  } else if (pathname === "/update") {
     //수정
-    topic.updateForm(response, request, queryData );
-  }
-  else if (pathname === "/update_process") {
+    topic.updateForm(response, request, queryData);
+  } else if (pathname === "/update_process") {
     //수정처리
     topic.updateProcess(response, request);
-  }
-  else if (pathname === "/process_delete") {
+  } else if (pathname === "/process_delete") {
     //삭제처리
     topic.deleteProcess(response, request);
-  }
-  else if (pathname === "/author") {
+  } else if (pathname === "/author") {
     //작성자관리로 이동
     author.home(response, request);
-  }
-  else if(pathname === "/author/create_process")
-  {
+  } else if (pathname === "/author/create_process") {
     //작성자 생성
     author.createProcess(response, request);
-  }
-  else if (pathname === "/author/update")
-  {
+  } else if (pathname === "/author/update") {
     //작성자 수정
     author.update(response, request, queryData);
-  }
-  else if (pathname === "/author/update_process"){
+  } else if (pathname === "/author/update_process") {
     // 작성자 수정 처리
     author.updateProcess(response, request);
-  }
-  else if (pathname === "/author/delete")
-  {
+  } else if (pathname === "/author/delete") {
     // 작성자 삭제 처리
     author.deleteProcess(response, request);
-  }
-  else {
+  } else {
     response.writeHead(404);
     response.end("Not Found");
   }
